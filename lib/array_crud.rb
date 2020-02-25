dd7492d5333e27a4f8b0cb0836441675e3ce1fda
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-25 13:12:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,13 +33,13 @@ array [index_number]
 end
 
 def retrieve_first_element_from_array(array)
-create_an_array [0]
+array [0]
 end
 
 def retrieve_last_element_from_array(array)
-  create_an_array [-1]
+  array [-1]
 end
 
 def update_element_from_index(array, index_number, element)
-create_an_array [4] 
+array [4] 
 end
