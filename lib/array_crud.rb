d6030bea8dd88ab1f31322771042622e80fff6f3
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-25 13:03:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,44 @@
+
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
+  ["name", "age", "sport", "level of education"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array.push  ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
   
+create_an_array.pop 
+
 end
 
 def remove_element_from_start_of_array(array)
+  create_an_array.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+create_an_array []
 end
 
 def retrieve_first_element_from_array(array)
-  
+create_an_array [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  create_an_array [-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+create_an_array [4] 
 end
